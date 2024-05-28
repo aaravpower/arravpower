27e18861ea4ed797f54cbd064d062b437dec99a4
aaravpower <aaravpower112000@gmail.com>
2024-05-28 20:45:53 +0530

    styles chnaegs

--- script.js
+++ script.js
@@ -5,29 +5,30 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider img');
+const totalSlides = slides.length;
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  slides[index].style.display = 'block';
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % totalSlides;
+  showSlide(slideIndex);
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + totalSlides) % totalSlides;
+  showSlide(slideIndex);
 }
+
+function autoSlide() {
+  nextSlide();
+}
+
+setInterval(autoSlide, 3000);
+
